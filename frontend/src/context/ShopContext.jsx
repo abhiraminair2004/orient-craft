3f40f620da2ff958d8dc6c3564fd4df589df5205
Abhirami Nair <abhiraminairmalu@gmail.com>
2025-06-25 17:32:47 +0530

    orders page in progress

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import {useNavigate} from 'react-router-dom'
 export const ShopContext= createContext();
 const ShopContextProvider= (props)=>{
     const currency='$';
@@ -7,6 +8,8 @@ const ShopContextProvider= (props)=>{
     const [search,setSearch]=useState('');
     const[showSearch,setShowSearch]=useState(false);
     const [cartItems,setCartItems]=useState({});
+    const navigate= useNavigate();
+
     const addToCart = async (itemId, size) => {
     let cartData = structuredClone(cartItems);
     if (cartData[itemId]) {
@@ -60,7 +63,7 @@ const ShopContextProvider= (props)=>{
     }
     const value={
         products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch,
-        cartItems, addToCart, getCartCount, updateQuantity, getCartAmount
+        cartItems, addToCart, getCartCount, updateQuantity, getCartAmount, navigate
     }
 
     return(
--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title'
 
 const Orders = () => {
+  const {products, currency}=useContext(ShopContext)
   return (
-    <div>Orders</div>
+    <div className='border-t pt-16'>
+      <div className="text-2xl">
+        <Title text1={'MY'} text2={'ORDERS'}/>
+      </div>
+      <div >
+        {
+          products.slice(1,4).map((item,index)=>(
+            <div className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:" key={index}></div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
