d0ec854677a0346dd323aa69b981662238f6a21f
Abhirami Nair <abhiraminairmalu@gmail.com>
2025-06-30 15:22:09 +0530

    frintend dine

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -12,7 +12,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className='text-gray-500'>63 Eighth Avenue, Woodville Gardens <br />Adelaide, SA 5012, Australia</p>
+          <p className='text-gray-500'>63 Eighth Avenue, Woodville Gardens <br />Adelaide, SA 5012, Australia
+          </p>
           <p className='font-semibold text-xl text-gray-600'>Contact Info:</p>
           <p className='text-gray-500'>Phone no.: +61-0405840398<br /> Email: admin@orientcraft.com</p>
         </div>
