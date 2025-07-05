7d636453cd0f41ff0774a39432ab38045e1f4e4b
Abhirami Nair <abhiraminairmalu@gmail.com>
2025-07-05 12:21:49 +0530

    produvt add list

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,13 @@
+import productModel from '../models/productModel.js'
 // function for adding product
 const addProduct = async (req, res) => {
   try {
     const { name, description, price, category, subCategory, sizes, bestseller, code } = req.body;
 
-    const image1 = req.files.image1[0];
-    const image2 = req.files.image2[0];
-    const image3 = req.files.image3[0];
-    const image4 = req.files.image4[0];
+    const image1 = req.files.image1||req.files.image1[0];
+    const image2 = req.files.image2||req.files.image2[0];
+    const image3 = req.files.image3||req.files.image3[0];
+    const image4 = req.files.image4||req.files.image4[0];
 
     console.log(name, description, price, category, subCategory, sizes, bestseller, code);
     console.log(image1, image2, image3, image4);
@@ -20,7 +21,13 @@ const addProduct = async (req, res) => {
 
 // function for list product
 const listProducts = async (req, res) => {
-  
+  try {
+    const products= await productModel.find({});
+    res.json({success:false, products})   
+  } catch (error) {
+    console.log(error)
+    res.json({success:false, message:error.message})
+  }
 }
 
 // function for removing product
