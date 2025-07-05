5bfcc1b3588e18a33c36e640f5ef9fdb10c60dcf
Abhirami Nair <abhiraminairmalu@gmail.com>
2025-07-05 17:05:16 +0530

    small change

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -9,8 +9,10 @@ const addProduct = async (req, res) => {
     const image3 = req.files.image3||req.files.image3[0];
     const image4 = req.files.image4||req.files.image4[0];
 
+    const images=[image1, image2, image3, image4].filter((item)=> item!==undefined)
+
     console.log(name, description, price, category, subCategory, sizes, bestseller, code);
-    console.log(image1, image2, image3, image4);
+    console.log(images);
 
     res.json({});
   } catch (error) {
