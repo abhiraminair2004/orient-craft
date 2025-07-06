d9ee941da521699e187bb47b59755a9479c7c395
Abhirami Nair <abhiraminairmalu@gmail.com>
2025-07-06 11:23:42 +0530

    images upload

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 import productModel from '../models/productModel.js'
+import {v2 as cloudinary} from "cloudinary"
 // function for adding product
 const addProduct = async (req, res) => {
   try {
@@ -10,6 +11,12 @@ const addProduct = async (req, res) => {
     const image4 = req.files.image4||req.files.image4[0];
 
     const images=[image1, image2, image3, image4].filter((item)=> item!==undefined)
+    let imageUrl= await Promise.all(
+      images.map(async(item)=>{
+        let result= await cloudinary.uploader.upload(item.path,{resource_type:'image'})
+        return result.secure_url
+      })
+    )
 
     console.log(name, description, price, category, subCategory, sizes, bestseller, code);
     console.log(images);
